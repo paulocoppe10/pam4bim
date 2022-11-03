2fe74156c19779b1c7750706bb997eb31101cfaf
paulocoppe10 <paulocoppe10@gmail.com>
2022-11-03 09:18:28 -0300

    erro nao mostra dados

--- components/Peruibe.js
+++ components/Peruibe.js
@@ -1,20 +1,32 @@
 import React,{useState, useEffect} from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, Touchable } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import Tempo from './Tempo';
+import api from  './Api';
 
 export default function Peruibe() {
- useEffect(
-     ()=>{
-         function teste(){
-             console.warn("Teste da userEffect");
-         }
-         teste();
-     },[]
- );
+    useEffect(
+        ()=>{
+            function teste(){
+                console.warn("Teste da userEffect");
+            }
+            teste();
+        },[]
+    );
+    const response = api.get  
+  const [dados, setDados] = useState("");
+  setDados(response.data.forecast[0]);
     return (
-    <View style={styles.bloco}>
-        <Text>Cidade de Peruibe</Text>
-    </View>
-   
+      <View style={styles.container}>
+        <View style={styles.bloco}>   
+          <Text style={styles.titulo}>Previsão do Tempo</Text>
+        </View>
+  
+        <View style={styles.blocoGeral}>
+          <Text style={styles.label}>Peruibe </Text>
+        </View>
+        
+        <Tempo data={dados}  />
+      </View>
   );
 }
 
